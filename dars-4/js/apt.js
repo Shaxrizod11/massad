60a7dfee9339adeea269476e90a0d6a7685a32ab
SHAXRIZOD <shaxrizod987@gmail.com>
2022-05-11 18:13:22 +0500

    nma gap endi

---
+++ dars-4/js/apt.js
@@ -0,0 +1,132 @@
+// let A =prompt(" Ani kiriting")
+// let B =prompt("B ni kiriting")
+// let C =prompt("C ni kiriting")
+//  let ayirma1 = A - B;
+//  let ayirma2 = A - C;
+
+//  if(ayirma1>ayirma2){
+//  console.log("B Yaqin")
+//  } else
+//  {
+// console.log("C yaqin")
+// }
+ 
+
+// let x=prompt(" x ni kirit")
+// let y=prompt(" y ni kiriting")
+
+// if(x>y){
+//     console.log(x + " " + "kootta")
+// } else{
+//     console.log(y + " " + "kotta")
+// }
+
+
+// let X = parseInt(prompt(" san kottasa birinchi san chiq"))
+// let Y = parseInt(prompt(" san kichkinasa ikkinchi  san chiq"))
+
+// if( X>Y){
+//     console.log(X)
+// } else if(X < Y){
+//     console.log(Y)
+// } else{
+//     console.log(" Son kiriting ")
+// }
+
+// if( X<Y){
+//     console.log(X)
+// } else if(X > Y){
+//     console.log(Y)
+// } else{
+//     console.log(" Son kiriting ")
+// }
+
+// let x=parseInt(prompt("1 kesma"))
+// let y=parseInt(prompt("2 kesma"))
+// let =r
+// let sanoq = 0
+// for(let i=y; i<=x; i=i+y){
+//     sanoq ++;
+//     r=i
+// }
+// alert(x-r + " " + "chiqadi")
+
+// Uy ishi: 
+// 1. Foydalanuvchi biror bir son kiritadi. Dasturingiz kiritilgan son toq yoki juft ekanligini aniqlashi kerak.
+// 2. Foydalanuvchi 3 ta son kiritadi. Shu uchta sonning ichidan eng kattasini ekranga chiqaring.
+// 3. Foydalanuvchi biror son kiritadi. Kiritilgan son 3 ga bo'linsa ekranga "FIZZ", 5 ga bo'linsa "Buzz", 3ga va 5ga bo'linsa esa "FIZZBUZZ" degan yozuv chiqsin.
+// 4. Foydalanuvchi yilni kiritadi. Dastur esa kiritilgan yil kabisa yilimi yoqmi aytishi kerak.
+
+// let son=parseInt(prompt("son kiriting biz uni toq yoki juft ekanligini aniqlaydi"))
+//  if(son%2){
+//      console.log("toq son")
+//  } else{
+//      console.log("juft son")
+//  }
+    
+
+// let son1 = parseInt(prompt("1 sonni kirit"))
+// let son2 = parseInt(prompt("2 sonni kirit"))
+// let son3 = parseInt(prompt("3 sonni kirit"))
+// let x;
+// if (son1>son2){
+//     x=son1
+//     console.log(son1)
+// }else {
+//     console.log(son2)
+// }
+//  if (x>son3){
+//      x=x
+//      console.log(x)
+// }else{
+//     console.log(son3)
+// }
+
+
+// let fizzbuzz = parseInt(prompt("3 va 5 soniga bo'linadigan son kiriting"))
+
+//   if( fizzbuzz % 3==0 &&  fizzbuzz % 5==0){
+//       console.log("FIZZBUZZ")
+//   } else if( fizzbuzz % 3==0){
+//       console.log("Fizz")
+//   } else if (fizzbuzz % 5==0){
+//      console.log("Buzz")
+//   } else{
+//     console.log(" eee sumalakka tuz somagan ahmoq promtga qanaqa shart yozilgan")
+//   }
+
+
+
+//  let a = parseInt(prompt(" A son kiriting"))
+//  let b = parseInt(prompt(" B son kiriting"))
+
+//  let k=0; 
+//  if(a%2==0) k=a+1
+//  if(a%2==1) k=a
+// let s=0
+//  for(let i = k; i<=b; i=i+2 ){
+//      s+=i;
+//  }
+//  console.log(s);
+
+// let n = prompt("n=")
+// let k = 1
+// for (let i=1; i<=n;i++){
+//     k*=i
+// }
+// console.log(k);
+
+// let a = prompt("a=")
+// let b = prompt("b=")
+// let c = prompt("c=")
+// let d = prompt("d=")
+// let e = prompt("e=")
+
+// let min = a
+// if(a>b) min = b
+// else if(min>c) min = c
+// else if (min > d) min=d
+// else if (min > e) min = e 
+
+// console.log(min); 
+
