993fc4b60a77c6889ca999f7de39e6bf7ac73ae3
SHAXRIZOD <shaxrizod987@gmail.com>
2022-05-11 17:03:13 +0500

    haloyiq

---
+++ dars-11/js/arr.js
@@ -0,0 +1,81 @@
+// let ul = document.createElement("ul")
+// let li = document.createElement("li")
+// li.innerText = "salom "
+// let img = document.createElement("img")
+// img.width = 200
+// img.height = 400
+// img.src="off.png"
+
+// let body= document.querySelector("body")
+
+// ul.appendChild(li)
+// body.appendChild(img)
+// body.appendChild(ul)
+
+// console.log(body);
+
+
+
+// let a_lar = document.createElement("a")
+// a_lar.innerText = "content"
+// let ul = document.createElement("ul")
+// let li = document.createElement ("li")
+// li.innerText = "atribut"
+// let img = document.createElement("img")
+// img.src="off.png"
+// img.width = 200
+
+
+// let body = document.querySelector("body")
+// ul.appendChild(li) 
+// body.appendChild(a_lar)
+// body.appendChild(ul)
+// body.appendChild(img)
+
+// console.log(body);
+
+
+
+
+
+
+// let div = document.createElement("div")
+// div.classList.add("card")
+
+
+// let img =document.createElement ("img")
+// img.src = "off.png"
+// img.alt = "Salom"
+// img.classList.add("rounded-circle")
+
+// let p_lar = document.createElement("p")
+// p_lar.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, a."
+
+// div.appendChild(img)
+// div.appendChild(p_lar)
+
+
+// document.querySelector("body").appendChild(div)
+// console.log(div) 
+
+ ////////////
+
+// let div = document.createElement("div")
+// div.classList.add("massad")
+
+// let a_lar = document.createElement("a")
+// a_lar.href = " https://www.google.com/search?q=kundalik+com&oq=kun&aqs=chrome.1.69i60j0i131i433i512j69i57j0i131i433i512j69i59j0i131i433i512j0i10i131i433j69i60.2495j0j7&sourceid=chrome&ie=UTF-8"
+// a_lar.innerHTML = "palakat"
+
+// let img = document.createElement("img")
+// img.src="off.png"
+
+
+// let p = document.createElement("p")
+// p.innerText = "bla bla bla"
+
+// div.appendChild(img)
+// div.appendChild(p)
+// div.appendChild(a_lar)
+// document.querySelector("body").appendChild(div)
+// console.log(div)
